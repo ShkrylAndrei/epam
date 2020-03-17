042f8b67270edbb2ae0422bbc3e64fd3000103eb
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-03-17 22:35:11 +0400

    Добавил файлик TestClass на всякий случай

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,8 @@
+
+//Такой откомпилированный класс должен лежать в папке C:\myClasses\
+public class TestClass {
+    @Override
+    public String toString() {
+        return "hello world";
+    }
+}
