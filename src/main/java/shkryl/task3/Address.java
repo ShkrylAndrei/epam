554ecf69ce76499355045cf853de640a5f9fab10
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-03-23 10:28:14 +0400

    Начал добавлять javadoc комментарии

--- src/main/java/shkryl/task3/Address.java
+++ src/main/java/shkryl/task3/Address.java
@@ -1,13 +1,21 @@
 package shkryl.task3;
 
-
-
+/**
+ * Описывает адрес для Human
+ */
 public class Address implements Comparable<Address>{
     private String city;
     private String street;
     private int house;
     private int room;
 
+    /**
+     * Конструктор инициализирует поля класса
+     * @param city название города
+     * @param street улица
+     * @param house номер дома
+     * @param room квартира
+     */
     public Address(String city, String street, int house, int room) {
         this.city = city;
         this.street = street;
@@ -47,6 +55,11 @@ public class Address implements Comparable<Address>{
         this.room = room;
     }
 
+    /**
+     * Сравнивает объекты класса Address на равенство
+     * @param o объект класса Address для сравнения
+     * @return возвращает true, если у обоих объектов совпадает city, street, house и room, иначе - false
+     */
     @Override
     public boolean equals(Object o){
         if (this==o) return true;
@@ -61,13 +74,20 @@ public class Address implements Comparable<Address>{
         return false;
     }
 
-
+    /**
+     * Генерирует hash code на основе city, street, house, room
+     * @return вовзращает сгенерированный hash code
+     */
     @Override
     public int hashCode() {
         return 31*(city.charAt(0)+street.charAt(0)+house+room);
 
     }
 
+    /**
+     * Формирует строку, описывающий объект класса Address, содержащую поля city, street, house, room
+     * @return возвращает сформированную строку
+     */
     @Override
     public String toString() {
         return "Address{" +
@@ -78,6 +98,13 @@ public class Address implements Comparable<Address>{
                 '}';
     }
 
+    /**
+     * Сравнивает два объекта класса Address на основе полей city, street, house, room для их сортировки
+     * @param o объект класса Address
+     * @return возвращает 0, если значения полей равны
+     * возвращает число больше нуля, если адрес в параметре больше текущего адреса
+     * в остальных случаях - число меньше нуля
+     */
     @Override
     public int compareTo(Address o) {
         int diff = city.compareTo(o.city);
--- src/main/java/shkryl/task3/ComparatorAddress.java
+++ src/main/java/shkryl/task3/ComparatorAddress.java
@@ -2,7 +2,16 @@ package shkryl.task3;
 
 import java.util.Comparator;
 
+/**
+ * Компаратор для сортировки Human на основе Address
+ */
 public class ComparatorAddress implements Comparator<Human> {
+    /**
+     * Сравнивает два объекта класса Human
+     * @param o1 объект Human
+     * @param o2 объект Human
+     * @return возвращает 0, если адреса объектов равны, число больше нуля, если адрес o1 больше o2, иначе - число меньше нуля
+     */
     @Override
     public int compare(Human o1, Human o2) {
         return o1.getAddress().compareTo(o2.getAddress());
