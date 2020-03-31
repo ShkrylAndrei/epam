0be4528763f0986cdbb14fdb195956bcd7c46d9e
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-03-31 10:37:59 +0400

    Еще изменения для 10го задания

--- src/main/java/shkryl/task10/Main.java
+++ src/main/java/shkryl/task10/Main.java
@@ -1,50 +1,30 @@
 package shkryl.task10;
 
 
+import shkryl.task10.entity.Car;
 import shkryl.task10.entity.Human;
 import shkryl.task10.logic.Service;
 
 import java.util.List;
 
 public class Main {
-    public Character aCharacter;
-
 
     public static void main(String[] args) {
-//через сервис:
+
         Service<Human> service = new Service<>();
         List<Human> list  = service.getInstance(Human.class);
         for(Human h : list) {
             System.out.println(h.getName() + " " + h.getAge());
         }
-//
-//        Service<Car> service2 = new Service<>();
-//        Car car = service2.getInstance(Car.class);
-//        System.out.println(car.getModel()+" "+car.getMaxSpeed());
+        System.out.println("####");
+        Service<Car> service2 = new Service<>();
+        List<Car> listCar = service2.getInstance(Car.class);
+        for(Car car : listCar) {
+           System.out.println(car.getModel() + " " + car.getMaxSpeed());
+        }
 
 
-//        Class<?> clazz = Human.class;
-//        if(Analyzer.checkEntityAnnotation(clazz)){
-//            try {
-//                Analyzer.checkNoValueAnnotationException(clazz);
-//                //временный try catch
-//                try {
-//                    Human human = (Human)Analyzer.initFieldsByAnnotaionValue(clazz);
-//                    System.out.println(human.getName()+" "+human.getAge());
-//
-//                }catch(Exception e){
-//                    e.printStackTrace();
-//                }
-//            }catch(NoValueAnnotationException e){
-//                System.out.println(e.toString());
-//            }
-//        }else{
-//            try{
-//                Analyzer.checkIllegalStateException(clazz);
-//            }catch(IllegalStateException e){
-//                System.out.println(e.toString());
-//            }
-//        }
+
     }
 
 }
