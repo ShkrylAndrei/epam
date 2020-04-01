00a767074f96429d41ccb6b7b700d241163916ae
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-04-01 06:14:25 +0400

    10 задания добавил логгер в главный класс

--- src/main/java/shkryl/task10/Main.java
+++ src/main/java/shkryl/task10/Main.java
@@ -1,26 +1,28 @@
 package shkryl.task10;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import shkryl.task10.entity.Car;
 import shkryl.task10.entity.Human;
 import shkryl.task10.logic.Service;
 import java.util.List;
 
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         Service<Human> service = new Service<>();
         List<Human> list = service.getInstance(Human.class);
         for (Human h : list) {
-            System.out.println(h.getName() + " " + h.getAge());
+            logger.info(h.getName() + " " + h.getAge());
         }
 
-        System.out.println();
-
+        logger.info("");
 
         Service<Car> service2 = new Service<>();
         List<Car> listCar = service2.getInstance(Car.class);
         for (Car car : listCar) {
-            System.out.println(car.getModel() + " " + car.getMaxSpeed());
+            logger.info(car.getModel() + " " + car.getMaxSpeed());
         }
     }
 }
