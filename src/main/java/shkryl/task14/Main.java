4c1a027f4974ab690db27b3f293e7ac834d30b8c
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-04-15 10:07:56 +0400

    Начал работу над 14 задачей

---
+++ src/main/java/shkryl/task14/Main.java
@@ -0,0 +1,13 @@
+package shkryl.task14;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+            logger.info("Message 1");
+            logger.info("Message 2");
+    }
+}
