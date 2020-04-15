acd435878278657f566f99c8d85c70d1b10dd9ed
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-04-15 10:34:43 +0400

    initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
-    public static void main(String[] args){
-        system.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 
 
